06f02a728a7d0b2606fc15fcadbd3b48ebe5ef43
eldrin <eldrin.rosales22@gmail.com>
2025-06-19 18:32:16 +0800

    added signup

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,10 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
 export function loginUser(email, password) {
   return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function signupUser(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
 }
