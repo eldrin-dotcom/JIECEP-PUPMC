83ea10d8d943c2ce3bb0454e5c320d494281481f
eldrin <eldrin.rosales22@gmail.com>
2025-06-19 18:41:12 +0800

    fix

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,10 +1,13 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
 export function loginUser(email, password) {
   return signInWithEmailAndPassword(auth, email, password);
 }
 
 export function signupUser(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password);
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      return sendEmailVerification(userCredential.user).then(() => userCredential);
+    });
 }
